297acf1747000e7f461e8eddccec6bcd3ddc9d25
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-15 21:19:35 +0100

    update default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
