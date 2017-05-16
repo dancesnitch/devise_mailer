dd54a92f1b76ea24a23e3b6baa4c60b91759f8fb
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-16 16:53:46 +0100

    added user edit, change to user login navbar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,27 @@
-class Registrationscontroller < Devise::Registrationscontroller
+class Devise::Registrationscontroller < DeviseController
 
 	private
 
-	def sign_up_params
-		params.require(:user).permit(
-			:email, 
-			:password, 
-			:password_confirmation
-			:username,
-			:firstname,
-			:lastname
-			)
+def sign_up_params
+	params.require(:user).permit(
+		:email, 
+		:password, 
+		:password_confirmation,
+		:username,
+		:firstname,
+		:lastname
+		)
 	end
 
-	def account_update_params
-		params.require(:user).permit(
-			:email, 
-			:password, 
-			:password_confirmation, 
-			:current_password
-			:username,
-			:firstname,
-			:lastname
-			)
+def account_update_params
+	params.require(:user).permit(
+		:email, 
+		:password, 
+		:password_confirmation, 
+		:current_password,
+		:username,
+		:firstname,
+		:lastname
+		)
 	end
 end	
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  	devise_for :users
 	root 'pages#index'
 
 	get 'pages/contact'
 
 	get 'pages/about'
-
 end
