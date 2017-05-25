44ed041d9153050b65bce91ffddaf6387ea12dc2
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-25 20:37:46 +0100

    added AWSS3, PaperClip actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,4 @@ Rails.application.routes.draw do
 	get 'pages/contact'
 
 	get 'pages/about'
-
-	resources :portfolios
 end
--- db/migrate/20170525144659_add_avatar_to_portfolios.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarToPortfolios < ActiveRecord::Migration[5.0]
-  def self.up
-    add_attachment :portfolios, :avatar
-  end
-
-  def self.down
-    remove_attachment :portfolios, :avatar
-  end
-end
