36eacdbc36cb0224ba1ccf33fc647e6cbf15b21b
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-28 00:11:45 +0100

    reinitalize

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+
+  def index
+  	@articles = Article.last(5)
+  end
+
+  def contact
+  	@contact= Contact.new
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  
+  	devise_for :users, :controllers => { registrations: 'registrations' }
+  	resources :articles
+	root 'pages#index'
+
+	get 'pages/contact'
+
+	get 'pages/about'
+
+	resources :contacts
+end
