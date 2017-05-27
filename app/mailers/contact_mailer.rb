0e00c2053449d220dee8d51506625986c349df4c
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-27 18:12:52 +0100

    add contact form and contact form attributes

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+default to: "byrne.daniel87@gmail.com"
+
+ def contact_email(name, email, message)
+	 @name = name
+	 @email = email
+	 @message = message
+
+ mail(from: email, subject: 'DanielJohnApps Contact Form Message')
+ end
+end
