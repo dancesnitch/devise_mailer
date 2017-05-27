df0c6ffc53cfcd2782851aaf2649234d7890f456
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-27 13:40:13 +0100

    dropped portfolio database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525185545) do
+ActiveRecord::Schema.define(version: 20170527123910) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,15 +18,6 @@ ActiveRecord::Schema.define(version: 20170525185545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-  end
-
-  create_table "portfolios", force: :cascade do |t|
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
