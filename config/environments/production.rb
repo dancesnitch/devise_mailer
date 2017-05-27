532a08334f05451801e08fae3b5533fc8c4ea848
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-27 14:06:51 +0100

    added production request

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,8 @@ Rails.application.configure do
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  #config/environments/production.rb
+  config.consider_all_requests_local = true # false
 end
 
 
