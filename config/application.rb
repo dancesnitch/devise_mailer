d271d1c7c23faa25175e1f9367d53ef9263e6c2e
Daniel Byrne <byrne.daniel87@gmail.com>
2017-05-30 15:05:03 +0100

    update to linguist app

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,14 @@ require_relative 'boot'
 require 'rails/all'
 require 'sprockets/railtie'
 
+require 'rugged'
+require 'linguist'
+
+repo = Rugged::Repository.new('.')
+project = Linguist::Repository.new(repo, repo.head.target_id)
+project.language       #=> "Ruby"
+project.languages      #=> { "Ruby" => 119387 }
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,13 +21,4 @@ module DeviseMailer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end 
-
-require 'rugged'
-require 'linguist'
-		#requried for gitlinguist
-		repo = Rugged::Repository.new('.')
-		project = Linguist::Repository.new(repo, repo.head.target_id)
-		project.language       #=> "Ruby"
-		project.languages      #=> { "Ruby" => 119387 }
-	end
 end
