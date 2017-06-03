3902e5ab15711cdd52a81958064fe5fd0ef4e882
Daniel Byrne <byrne.daniel87@gmail.com>
2017-06-03 21:04:29 +0100

    added to production config dynopoker

--- config/dynopoker.rb
+++
@@ -1,5 +0,0 @@
-Dynopoker.configure do |config|
-	config.address = 'http://wakemydyno.com'
-#  config.enable = false # default is true
-#  config.poke_frequency = 123 # default is 1800s (30min)
-end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://danieljohnportfolio.herokuapp.com' }
 end
 
-
+Dynopoker.configure do |config|
+  config.address = 'http://wakemydyno.com'
+#  config.enable = false # default is true
+#  config.poke_frequency = 123 # default is 1800s (30min)
+end
 
 
